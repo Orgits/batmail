17356fcc5b8a413565878a8eebb35d25a9af561e
Anubhav Sharma <anubhav@Anubhavs-MacBook-Air.local>
2025-01-19 15:39:49 +0530

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,10 +134,10 @@ export default function Example() {
                 href="#"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Start today
               </a>
               <a href="#" className="text-sm/6 font-semibold text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
+                Learn more<span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
